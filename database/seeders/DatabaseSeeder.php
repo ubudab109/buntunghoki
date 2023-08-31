a75afa9867e79631f8947ca6edd282634a997408
Muhammad Rizky Firdaus <rizkyfirdaus0309@gmail.com>
2023-09-01 02:15:19 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            CompanySetting::class,
+            CompanySettingSeeder::class,
             RolePermissionSeeder::class,
             UserSeeder::class,
         ]);
